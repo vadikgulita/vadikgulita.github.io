a41f5b358cd3e18be78ae539ca18d26b68e8a8b1
vadikgulita <vadikgulita@users.noreply.github.com>
2019-08-08 03:32:06 +0300

    Update video-autoplay.js

--- smiley/js/video-autoplay.js
+++ smiley/js/video-autoplay.js
@@ -1,6 +1,19 @@
-$(document).on('click', '.video-overlay', function() {
-	var $video = $('#video'),
-		src = $video.attr('src');
- 
-	$video.attr('src', src + '?autoplay=1');
-});
+var video = document.getElementById('myVideo');
+var playbutton = document.getElementById("play");
+
+playbutton.addEventListener("click", function (e) {
+  //  Toggle between play and pause based on the paused property
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}, false);
+
+video.addEventListener("play", function () {
+  playbutton.innerHTML = "Pause";
+}, false);
+
+video.addEventListener("pause", function () {
+  playbutton.innerHTML = "Play";
+}, false);
