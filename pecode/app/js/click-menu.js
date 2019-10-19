6110bf120ce1927fcf95d8afe38ceef285668fef
vadikgulita <vadikgulita@users.noreply.github.com>
2019-10-19 04:57:22 +0300

    додав адаптивність

---
+++ pecode/app/js/click-menu.js
@@ -0,0 +1,8 @@
+$("#menu").on("click","a", function (event) {
+    
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    
+    $('body,html').animate({scrollTop}, 1500);
+});
