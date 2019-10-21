82609750bfda4743a5e75b3c370776efff5ec846
vadikgulita <vadikgulita@users.noreply.github.com>
2019-10-22 00:24:29 +0300

    add prefix for style

--- pecode/app/js/click-menu.js
+++ pecode/app/js/click-menu.js
@@ -1,8 +1,8 @@
-$("#menu").on("click","a", function (event) {
-    
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-    top = $(id).offset().top;
-    
-    $('body,html').animate({scrollTop}, 1500);
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
